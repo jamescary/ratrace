04e684ed72b418b2911ee6e839043bb7cf0d045c
James Cary <eliteeggnog@gmail.com>
2014-04-10 22:20:29 -0400

    fixed error in Votes collection model, allows multiple instances of Game to be played at same time; broke secret anarchy mode :(

--- client/ratrace.js
+++ client/ratrace.js
@@ -1,9 +1,5 @@
 // Main client application logic //
 
-var ADJACENCIES = [
-[10, 1], [11, 0, 2], [12, 1, 3], [13, 2, 4], [14, 3, 5], [15, 4, 6], [16, 5, 7], [17, 6, 8], [18, 7, 9], [19, 8], [0, 20, 2], [1, 21, 1, 3], [2, 22, 2, 4], [3, 23, 3, 5], [4, 24, 4, 6], [5, 25, 5, 7], [6, 26, 6, 8], [7, 27, 7, 9], [8, 28, 8, 10], [9, 29, 9], [10, 30, 3], [11, 31, 2, 4], [12, 32, 3, 5], [13, 33, 4, 6], [14, 34, 5, 7], [15, 35, 6, 8], [16, 36, 7, 9], [17, 37, 8, 10], [18, 38, 9, 11], [19, 39, 10], [20, 40, 4], [21, 41, 3, 5], [22, 42, 4, 6], [23, 43, 5, 7], [24, 44, 6, 8], [25, 45, 7, 9], [26, 46, 8, 10], [27, 47, 9, 11], [28, 48, 10, 12], [29, 49, 11], [30, 50, 5], [31, 51, 4, 6], [32, 52, 5, 7], [33, 53, 6, 8], [34, 54, 7, 9], [35, 55, 8, 10], [36, 56, 9, 11], [37, 57, 10, 12], [38, 58, 11, 13], [39, 59, 12], [40, 60, 6], [41, 61, 5, 7], [42, 62, 6, 8], [43, 63, 7, 9], [44, 64, 8, 10], [45, 65, 9, 11], [46, 66, 10, 12], [47, 67, 11, 13], [48, 68, 12, 14], [49, 69, 13], [50, 70, 7], [51, 71, 6, 8], [52, 72, 7, 9], [53, 73, 8, 10], [54, 74, 9, 11], [55, 75, 10, 12], [56, 76, 11, 13], [57, 77, 12, 14], [58, 78, 13, 15], [59, 79, 14], [60, 80, 8], [61, 81, 7, 9], [62, 82, 8, 10], [63, 83, 9, 11], [64, 84, 10, 12], [65, 85, 11, 13], [66, 86, 12, 14], [67, 87, 13, 15], [68, 88, 14, 16], [69, 89, 15], [70, 90, 9], [71, 91, 8, 10], [72, 92, 9, 11], [73, 93, 10, 12], [74, 94, 11, 13], [75, 95, 12, 14], [76, 96, 13, 15], [77, 97, 14, 16], [78, 98, 15, 17], [79, 99, 16], [80, 10], [81, 9, 11], [82, 10, 12], [83, 11, 13], [84, 12, 14], [85, 13, 15], [86, 14, 16], [87, 15, 17], [88, 16, 18], [89, 17]
-];
-
 //
 // Utility functions
 //
@@ -18,7 +14,9 @@ var game = function () {
 };
 
 var vote = function (direction) {
-    var vote = Votes.findOne({direction: direction});
+    var me = player();
+    var vote = Votes.findOne({game_id: me.game_id,
+                              direction: direction});
     console.log("dir: "+direction);
 
     if (vote) {
@@ -26,49 +24,17 @@ var vote = function (direction) {
         console.log("setting dir: "+direction+" to count "+count);
         var result = Votes.update({_id: vote._id}, {$set: {count: count}});
     } else {
-        Votes.insert({direction: direction,
+        Votes.insert({game_id: me.game_id,
+                      direction: direction,
                       count: 1});
     }
 
-    var vote = Votes.findOne({direction: direction});
+    var vote = Votes.findOne({game_id: me.game_id,
+                              direction: direction});
     console.log(vote);
 
     return;
 };
-
-// var move = function (direction) {
-//     if (!game()) {
-//         console.log("error finding game.");
-//         return;
-//     }
-//     var board = game().board;
-//     var mouseLoc = board.indexOf('M');
-//     var cheeseLoc = board.indexOf('C');
-//
-//     if (direction == 'left') {
-//         if (ADJACENCIES[mouseLoc].indexOf(mouseLoc - 1) != -1) {
-//             board[mouseLoc - 1] = 'M';
-//             board[mouseLoc] = '.';
-//         }
-//     } else if (direction == 'right') {
-//         if (ADJACENCIES[mouseLoc].indexOf(mouseLoc + 1) != -1) {
-//             board[mouseLoc + 1] = 'M';
-//             board[mouseLoc] = '.';
-//         }
-//     } else if (direction == 'down') {
-//         if (ADJACENCIES[mouseLoc].indexOf(mouseLoc + 10) != -1) {
-//             board[mouseLoc + 10] = 'M';
-//             board[mouseLoc] = '.';
-//         }
-//     } else if (direction == 'up') {
-//         if (ADJACENCIES[mouseLoc].indexOf(mouseLoc - 10) != -1) {
-//             board[mouseLoc - 10] = 'M';
-//             board[mouseLoc] = '.';
-//         }
-//     }
-//
-//     Games.update(game().game_id, {$set: {board: board}});
-// };
 
 //
 // lobby template: shows all users not currently playing, and
@@ -165,10 +131,6 @@ Template.board.events({
 // voting board
 //
 
-Template.voteboard.show = function () {
-    return game() && game().gameclock > 0;
-};
-
 Template.voteboard.events({
     // var me = player();
 
@@ -204,13 +166,17 @@ Template.voteboard.events({
     }
 });
 
+Template.voteboard.show = function() {
+    return game() && game().voteclock >= 0;
+};
+
 Template.voteboard.votes = function() {
     return Votes.find({game_id: game() && game()._id});
 };
 
 Template.voteboard.voteclock = function() {
     if (!game()) {
-        return '0:00';
+        return '';
     }
 
     return game().voteclock;
@@ -226,48 +192,6 @@ Template.postgame.show = function () {
 Template.postgame.events({
     'click button': function (evt) {
         Players.update(Session.get('player_id'), {$set: {game_id: null}});
-    }
-});
-
-Meteor.methods({
-    move: function (direction) {
-        if (!game()) {
-            console.log("error finding game.");
-            return;
-        }
-        var board = game().board;
-        var me = player();
-        var mouseLoc = board.indexOf('M');
-        var cheeseLoc = board.indexOf('C');
-
-        console.log("ADJACENCIES[0]: "+ADJACENCIES[0]);
-
-        if (direction == 'left') {
-            if (ADJACENCIES[mouseLoc].indexOf(mouseLoc - 1) != -1) {
-                board[mouseLoc - 1] = 'M';
-                board[mouseLoc] = '.';
-            }
-        } else if (direction == 'right') {
-            if (ADJACENCIES[mouseLoc].indexOf(mouseLoc + 1) != -1) {
-                console.log("trying to move M to right");
-                board[mouseLoc + 1] = 'M';
-                board[mouseLoc] = '.';
-            }
-        } else if (direction == 'down') {
-            if (ADJACENCIES[mouseLoc].indexOf(mouseLoc + 10) != -1) {
-                board[mouseLoc + 10] = 'M';
-                board[mouseLoc] = '.';
-            }
-        } else if (direction == 'up') {
-            if (ADJACENCIES[mouseLoc].indexOf(mouseLoc - 10) != -1) {
-                board[mouseLoc - 10] = 'M';
-                board[mouseLoc] = '.';
-            }
-        }
-
-        Games.update(me.game_id, {$set: {board: board}});
-
-        console.log("board: "+game().board);
     }
 });
 
